557224fd7ea80489af0d5dd2d7e71cabe93d533a
Mary Strodl <ipadlover8322@gmail.com>
2022-02-21 15:44:36 -0500

    GateKeeperMemberListener: allow realm selection

--- src/lib.rs
+++ src/lib.rs
@@ -24,7 +24,10 @@ pub struct GateKeeperMemberListener<'a> {
     endpoint: String,
 
     // Safeguard against double-scans
-    just_scanned: bool
+    just_scanned: bool,
+
+    // Route URL
+    route: &'static str,
 }
 
 pub enum FetchError {
@@ -34,18 +37,67 @@ pub enum FetchError {
     Unknown,
 }
 
+pub enum RealmInfo {
+    Drink,
+    MemberProjects,
+    Doors,
+}
+
+impl RealmInfo {
+    fn get_route(&self) -> &'static str {
+        match self {
+            RealmInfo::Drink => "drink",
+            RealmInfo::Doors => "doors",
+            RealmInfo::MemberProjects => "projects",
+        }
+    }
+    fn get_name(&self) -> &'static str {
+        match self {
+            RealmInfo::Doors => "Doors",
+            RealmInfo::Drink => "Drink",
+            RealmInfo::MemberProjects => "Member Projects",
+        }
+    }
+    fn env_name(&self) -> &'static str {
+        match self {
+            RealmInfo::Drink => "DRINK",
+            RealmInfo::Doors => "DOORS",
+            RealmInfo::MemberProjects => "MEMBER_PROJECTS",
+        }
+    }
+    fn get_auth_key(&self) -> String {
+        env::var(format!("GK_REALM_{}_AUTH_KEY", self.env_name())).unwrap()
+    }
+    fn get_read_key(&self) -> String {
+        env::var(format!("GK_REALM_{}_READ_KEY", self.env_name())).unwrap()
+    }
+    fn get_public_key(&self) -> String {
+        env::var(format!("GK_REALM_{}_PUBLIC_KEY", self.env_name())).unwrap()
+    }
+    fn get_id(&self) -> u8 {
+        match self {
+            RealmInfo::Doors => 0,
+            RealmInfo::Drink => 1,
+            RealmInfo::MemberProjects => 2,
+        }
+    }
+}
+
 impl <'a> GateKeeperMemberListener<'a> {
-    pub fn new(nfc: &'a mut Nfc, conn_str: String) -> Option<Self> {
+    pub fn new_for_realm(nfc: &'a mut Nfc, conn_str: String, realm_detail: RealmInfo) -> Option<Self> {
         let nfc_device = nfc.gatekeeper_device(conn_str)
             .ok_or("failed to get gatekeeper device").unwrap();
 
         let realm = Realm::new(
-            2, "Member Projects", "",
-            &env::var("GK_REALM_MEMBER_PROJECTS_AUTH_KEY").unwrap().to_string(),
-            &env::var("GK_REALM_MEMBER_PROJECTS_READ_KEY").unwrap().to_string(),
-            &env::var("GK_REALM_MEMBER_PROJECTS_UPDATE_KEY").unwrap().to_string(),
-            &env::var("GK_REALM_MEMBER_PROJECTS_PUBLIC_KEY").unwrap().to_string(),
-            &env::var("GK_REALM_MEMBER_PROJECTS_PRIVATE_KEY").unwrap().to_string()
+            realm_detail.get_id(), realm_detail.get_name(), "",
+            &realm_detail.get_auth_key(),
+            &realm_detail.get_read_key(),
+            // No write key:
+            &"a".repeat(32),
+            &realm_detail.get_public_key(),
+            // No private key:
+            &format!("-----BEGIN EC PRIVATE KEY-----\
+{}\n-----END EC PRIVATE KEY-----\n", "a".repeat(224))
         ).unwrap();
 
         return Some(GateKeeperMemberListener {
@@ -56,8 +108,13 @@ impl <'a> GateKeeperMemberListener<'a> {
             server_token: env::var("GK_SERVER_TOKEN").unwrap().to_string(),
             just_scanned: false,
             endpoint: env::var("GK_HTTP_ENDPOINT")
-                .unwrap_or("http://localhost:3000".to_string()).to_string()
+                .unwrap_or("http://localhost:3000".to_string()).to_string(),
+            route: realm_detail.get_route(),
         });
+    }
+
+    pub fn new(nfc: &'a mut Nfc, conn_str: String) -> Option<Self> {
+        Self::new_for_realm(nfc, conn_str, RealmInfo::MemberProjects)
     }
 
     pub fn poll_for_user(&mut self) -> Option<String> {
@@ -90,7 +147,7 @@ impl <'a> GateKeeperMemberListener<'a> {
 
     pub fn fetch_user(&mut self, key: String) -> Result<Value, FetchError> {
         match self.http.get(
-            self.endpoint.clone() + "/projects/by-key/" + &key
+            format!("{}/{}/by-key/{}", self.endpoint.clone(), self.route, &key)
         ).header(AUTHORIZATION, self.server_token.clone()).send() {
             Ok(res) => {
                 match res.status() {
